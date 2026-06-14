ba81ad61bdc9af6515a3a899c0806aec6dcffdf7
mohammad-sagher <mohammadssagher@gmail.com>
2026-06-14 20:32:52 +0300

    Add Firebase configuration for the project

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCg7tt1ZNyCu052ER7QETecNrbtnqOjzsU",
+  authDomain: "car-security-app-e8b94.firebaseapp.com",
+  databaseURL: "https://car-security-app-e8b94-default-rtdb.firebaseio.com",
+  projectId: "car-security-app-e8b94",
+  storageBucket: "car-security-app-e8b94.firebasestorage.app",
+  messagingSenderId: "614504432857",
+  appId: "1:614504432857:web:a914a36f920f6f9de1eac8",
+  measurementId: "G-DX9B5TBE2Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
